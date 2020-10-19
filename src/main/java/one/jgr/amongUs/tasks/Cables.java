bc7e253c8baa822a61f7446b413f2cb5ce014f62
Joshua Benning <jabenning13@gmail.com>
2020-10-19 23:48:57 +0200

    added Cables.java

---
+++ src/main/java/one/jgr/amongUs/tasks/Cables.java
@@ -0,0 +1,21 @@
+package one.jgr.amongUs.tasks;
+
+import net.wargearworld.GUI_API.GUI.ChestGUI;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class Cables extends Task{
+
+    public Cables(Player player) {
+        super(player);
+    }
+
+    public void startTask() {
+
+    }
+
+    public ChestGUI createInv() {
+        ChestGUI cable_gui = new ChestGUI(45, "Test");
+        return cable_gui;
+    }
+}
