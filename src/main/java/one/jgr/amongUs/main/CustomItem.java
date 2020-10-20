051ed0139826cc2710d6420b8db7e1e2b16190c1
larsjgr <larsjgr@gmail.com>
2020-10-20 18:31:39 +0200

    extended CustomItem to accommodate stacking of items

--- src/main/java/one/jgr/amongUs/main/CustomItem.java
+++ src/main/java/one/jgr/amongUs/main/CustomItem.java
@@ -1,4 +1,4 @@
-package one.jgr.one.main;
+package one.jgr.amongUs.main;
 
 import one.jgr.amongUs.main.Main;
 import org.bukkit.Material;
@@ -14,16 +14,16 @@ import java.util.ArrayList;
             public ItemStack getItemStack(String languageCode) {
                 switch (this) {
                     case CHALLENGE:
-                        return createItemStack(Material.GOLDEN_SWORD, Main.getString(languageCode, "item_challenge"), Main.getString(languageCode, "item_lore_challenge"));
+                        return createItemStack(1, Material.GOLDEN_SWORD, Main.getString(languageCode, "item_challenge"), Main.getString(languageCode, "item_lore_challenge"));
                     case INVITE:
-                        return createItemStack(Material.LEAD, Main.getString(languageCode, "item_invite"), Main.getString(languageCode, "item_lore_invite"));
+                        return createItemStack(1, Material.LEAD, Main.getString(languageCode, "item_invite"), Main.getString(languageCode, "item_lore_invite"));
                     case STATS:
-                        return createItemStack(Material.MAP, Main.getString(languageCode, "item_stats"), Main.getString(languageCode, "item_lore_stats"));
+                        return createItemStack(1, Material.MAP, Main.getString(languageCode, "item_stats"), Main.getString(languageCode, "item_lore_stats"));
                 }
                 return null;
             }
 
-            public static ItemStack createItemStack(Material customMaterial, String customItemDisplayName, String customItemLore) {
+            public static ItemStack createItemStack(int amount, Material customMaterial, String customItemDisplayName, String customItemLore) {
                 ItemStack currentItemStack = new ItemStack(customMaterial);
                 ItemMeta currentItemMeta = currentItemStack.getItemMeta();
                 currentItemMeta.setDisplayName(customItemDisplayName);
