2308198a2e135ac5684186dc6e3d57c1af7b7f3d
larsjgr <larsjgr@gmail.com>
2020-10-24 17:53:19 +0200

    added initialisation of emergency button added method teleportPlayers() that teleports everybody to the gameSpawns and makes them face the emergency button

--- src/main/java/one/jgr/amongUs/game/Map.java
+++ src/main/java/one/jgr/amongUs/game/Map.java
@@ -8,6 +8,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.data.AnaloguePowerable;
 import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_15_R1.block.CraftBlock;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 
@@ -22,8 +23,8 @@ public enum Map {
     private Location mapBorder2;
     private Location lobbySpawn;
     private Location lights;
+    private Location emergencyButton;
     private ArrayList<Location> gameSpawns = new ArrayList<>();
-    private ArrayList<VentSystem> ventsystems;
 
     Map (String name, int x1, int y1, int z1, int x2, int y2, int z2) {
         this.name = name;
@@ -57,6 +58,10 @@ public enum Map {
                     if(w.getBlockAt(x,y,z).getType().equals(Material.GOLD_BLOCK) && w.getBlockAt(x,y-1,z).getType().equals(Material.REDSTONE_BLOCK)) {
                         lights = new Location(w, x, y+1, z);
                     }
+                    // initialising emergency button
+                    if(w.getBlockAt(x,y,z).getType().equals(Material.GOLD_BLOCK) && w.getBlockAt(x,y-1,z).getType().equals(Material.STONE_BUTTON)) {
+                        emergencyButton = new Location(w, x, y+1, z);
+                    }
                     // if needed add visual task locations, e. g. scanner
                 }
             }
@@ -89,4 +94,51 @@ public enum Map {
         ((CraftWorld) block.getWorld()).getHandle().applyPhysics(
                 new BlockPosition(block.getX(), block.getY(), block.getZ()), ((CraftBlock) block).getNMS().getBlock());
     }
+    public void teleportPlayers() {
+        int counter = 0;
+        for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+            if(PlayerColor.getPlayerColor(p) != null) {
+                // teleports player on one game spawn each
+                p.teleport(this.gameSpawns.get(counter));
+                // makes player face the emergency button
+                p.teleport(lookAt(p.getLocation(), emergencyButton));
+                counter ++;
+            }
+        }
+    }
+    private Location lookAt(Location loc, Location lookat) {
+        //Clone the loc to prevent applied changes to the input loc
+        loc = loc.clone();
+
+        // Values of change in distance (make it relative)
+        double dx = lookat.getX() - loc.getX();
+        double dy = lookat.getY() - loc.getY();
+        double dz = lookat.getZ() - loc.getZ();
+
+        // Set yaw
+        if (dx != 0) {
+            // Set yaw start value based on dx
+            if (dx < 0) {
+                loc.setYaw((float) (1.5 * Math.PI));
+            } else {
+                loc.setYaw((float) (0.5 * Math.PI));
+            }
+            loc.setYaw((float) loc.getYaw() - (float) Math.atan(dz / dx));
+        } else if (dz < 0) {
+            loc.setYaw((float) Math.PI);
+        }
+
+        // Get the distance from dx/dz
+        double dxz = Math.sqrt(Math.pow(dx, 2) + Math.pow(dz, 2));
+
+        // Set pitch
+        loc.setPitch((float) -Math.atan(dy / dxz));
+
+        // Set values, convert to degrees (invert the yaw since Bukkit uses a different yaw dimension format)
+        loc.setYaw(-loc.getYaw() * 180f / (float) Math.PI);
+        loc.setPitch(loc.getPitch() * 180f / (float) Math.PI);
+
+        return loc;
+    }
+
 }
